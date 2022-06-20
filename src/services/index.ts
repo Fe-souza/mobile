c30f52651f351ba843993abcbf70f859bfda74ab
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-20 16:34:06 -0300

    type useAddData and addData

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,81 +1,79 @@
-import firestore from '@react-native-firebase/firestore';
-import storage from '@react-native-firebase/storage';
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
 
-export function uploadImage(path:string,image:string){
-    const fileName = new Date().getTime();
-    const reference = storage().ref(`/${path}/${fileName}.png`);
+export function uploadImage(path: string, image: string) {
+  const fileName = new Date().getTime();
+  const reference = storage().ref(`/${path}/${fileName}.png`);
 
-    reference.putFile(image);
-    return reference.getDownloadURL();
-
+  reference.putFile(image);
+  return reference.getDownloadURL();
 }
 
-export async function deleteImage(photoPath:string){
-
-    const success = await storage()
+export async function deleteImage(photoPath: string) {
+  const success = await storage()
     .ref(photoPath)
     .delete()
     .then(() => {
-        return true
-      })
-      .catch(() => {
-        return false
-      });
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
 
-      return success
-
+  return success;
 }
 
-export async function addData(data:any){
-
+export interface InterfaceExample {
+  collection: [string];
+}
+//TODO: tipar essa data aqui para que possa
+//usar a interface no useAddData
+export async function addData(data: InterfaceExample): Promise<boolean> {
   const collection = data.collection;
   delete data.collection;
 
-    const success = await firestore()
-      .collection(collection)
-      .add(data)
-      .then(() => {
-        return true
-      })
-      .catch(() => {
-        return false
-      });
+  const success = await firestore()
+    .collection(collection)
+    .add(data)
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
 
-      return success
+  return success;
 }
 
-export async function getData(collection:string){
-    const data = await firestore()
+export async function getData(collection: string) {
+  const data = await firestore()
     .collection(collection)
     .get()
     .then((response) => {
-         
-        const list = response.docs.map(doc => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          }
-        })
-        
-      return list;
-
-    })
-   
-    return data
-   
-}
-
-export async function deleteData(collection:string,id:string){
-    const success = await firestore()
-      .collection(collection)
-      .doc(id)
-      .delete()
-      .then(() => {
-        return true
-      })
-      .catch(() => {
-        return false
+      const list = response.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
       });
 
-      return success
+      return list;
+    });
+
+  return data;
+}
+
+export async function deleteData(collection: string, id: string) {
+  const success = await firestore()
+    .collection(collection)
+    .doc(id)
+    .delete()
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+
+  return success;
 }
